f557b542d92b047693bdd52296207f0b30ac5a48
CheezyMac <c.macleod@hotmail.ca>
2018-03-15 13:43:54 -0400

    + Added implementations for cardinal precepts - Removed call to unimplemented communication class

--- controller.py
+++ controller.py
@@ -1,5 +1,5 @@
 import numpy as np
-import communication
+#import communication
 
 class Controller:
     type = "undefined"
@@ -8,7 +8,7 @@ class Controller:
         self.visited = np.full((body.env.y_upper - body.env.y_lower), (body.env.x_upper - body.env.x_lower), False)
         self.faction = faction
         self.body = body
-        self.private_channel = communication.PrivateChannel()
+        #self.private_channel = communication.PrivateChannel()
 
     def getType(self):
         return self.type
@@ -21,19 +21,100 @@ class Controller:
 
     # Return True if unobserved cells above agent
     def perceiveAbove(self):
-        return None
+        #Get position
+        x_pos, y_pos = self.getPosition()
+
+        #Ensure x_low within environment boundary
+        if (x_pos - 10) >= self.body.env.x_lower:
+            x_low = x_pos - 10
+        else:
+            x_low = self.body.env.x_lower
+
+        #Ensure x_high within environment boundary
+        if (x_pos + 10) <= self.body.env.x_upper:
+            x_high = x_pos + 10
+        else:
+            x_high = self.body.env.x_upper
+
+
+        #Check upwards until unvisited element or boundary
+        for y_observing in range(y_pos, self.body.env.y_upper):
+            for x_observing in range(x_low, x_high):
+                if not self.visited[x_observing, y_observing]:
+                    return True
+        return False
 
     # Return True if unobserved cells below agent
     def perceiveBelow(self):
-        return None
+        # Get position
+        x_pos, y_pos = self.getPosition()
+
+        # Ensure x_low within environment boundary
+        if (x_pos - 10) >= self.body.env.x_lower:
+            x_low = x_pos - 10
+        else:
+            x_low = self.body.env.x_lower
+
+        # Ensure x_high within environment boundary
+        if (x_pos + 10) <= self.body.env.x_upper:
+            x_high = x_pos + 10
+        else:
+            x_high = self.body.env.x_upper
+
+        # Check downwards until unvisited element or boundary
+        for y_observing in range(y_pos, self.body.env.y_upper, -1):
+            for x_observing in range(x_low, x_high):
+                if not self.visited[x_observing, y_observing]:
+                    return True
+        return False
 
     # Return True if unobserved cells left of agent
     def perceiveLeft(self):
-        return None
+        #Get position
+        x_pos, y_pos = self.getPosition()
+
+        #Ensure y_low within environment boundary
+        if (y_pos - 10) >= self.body.env.y_lower:
+            y_low = y_pos - 10
+        else:
+            y_low = self.body.env.y_lower
+
+        #Ensure y_high within element boundary
+        if (y_pos + 10) <= self.body.env.y_upper:
+            y_high = y_pos + 10
+        else:
+            y_high = self.body.env.y_upper
+
+        #Check left until unvisited element or boundary
+        for x_observing in range(x_pos, self.body.env.x_lower, -1):
+            for y_observing in range(y_low, y_high):
+                if not self.visited[x_observing, y_observing]:
+                    return True
+        return False
 
     # Return True if unobserved cells right of agent
     def perceiveRight(self):
-        return None
+        # Get position
+        x_pos, y_pos = self.getPosition()
+
+        # Ensure y_low within environment boundary
+        if (y_pos - 10) >= self.body.env.y_lower:
+            y_low = y_pos - 10
+        else:
+            y_low = self.body.env.y_lower
+
+        # Ensure y_high within element boundary
+        if (y_pos + 10) <= self.body.env.y_upper:
+            y_high = y_pos + 10
+        else:
+            y_high = self.body.env.y_upper
+
+        # Check right until unvisited element or boundary
+        for x_observing in range(x_pos, self.body.env.x_lower):
+            for y_observing in range(y_low, y_high):
+                if not self.visited[x_observing, y_observing]:
+                    return True
+        return False
 
     #Get a list of all entities around the body
     def perceiveRadar(self):
