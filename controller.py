49735f5805e43d400acf057af845238367be1988
CheezyMac <c.macleod@hotmail.ca>
2018-03-15 13:20:24 -0400

    + Added CompetitiveController framework + Added precept frameworks to Controller for sensing presence of empty cells cardinal to body + Added runTurn() methods to agents; placeholders return None

--- controller.py
+++ controller.py
@@ -1,11 +1,14 @@
 import numpy as np
+import communication
 
 class Controller:
     type = "undefined"
+    memory = {}
     def __init__(self, faction, body):
         self.visited = np.full((body.env.y_upper - body.env.y_lower), (body.env.x_upper - body.env.x_lower), False)
         self.faction = faction
         self.body = body
+        self.private_channel = communication.PrivateChannel()
 
     def getType(self):
         return self.type
@@ -15,6 +18,22 @@ class Controller:
 
     def getPosition(self):
         return self.body.x, self.body.y
+
+    # Return True if unobserved cells above agent
+    def perceiveAbove(self):
+        return None
+
+    # Return True if unobserved cells below agent
+    def perceiveBelow(self):
+        return None
+
+    # Return True if unobserved cells left of agent
+    def perceiveLeft(self):
+        return None
+
+    # Return True if unobserved cells right of agent
+    def perceiveRight(self):
+        return None
 
     #Get a list of all entities around the body
     def perceiveRadar(self):
@@ -35,13 +54,47 @@ class Controller:
         return self.body.scan()
 
     def perceiveMessage(self, message):
-        #TODO: Implement message receive in inherited controllers
+        return None
+
+    def runTurn(self):
         return None
 
 
 
 class CollaborativeController(Controller):
     type = "collaborative_agent"
+
+    def perceiveMessage(self, message):
+        #Do the things, perceive the message
+        return None
+
+    def runTurn(self):
+        #Do the stuff!
+        visible = self.perceiveRadar()
+        action_selected = ["nothing", 0] #Action code, action priority
+        if len(visible) == 0:
+            #Nothing seen
+            return None
+        else:
+            #Found something, categorize and adjust behaviour
+            return None
+
+
+
+
+
+
+
+class CompetitiveController(Controller):
+    type = "competitive_agent"
+
+    def perceiveMessage(self, message):
+        #Need to do something with this, gonna be global
+        return None
+
+    def runTurn(self):
+        return None
+
 
 
 class TargetController(Controller):
