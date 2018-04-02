90fef66a25cd1af97a679c9e1c40a9bfc0e38715
CheezyMac <c.macleod@hotmail.ca>
2018-04-02 09:00:59 -0400

    + Complete overhaul of communication system

--- communication.py
+++ communication.py
@@ -1,31 +1,21 @@
-
-class CommunicationChannel:
-    message_log = []
-    #TODO: Controller should only have 2 instances of CommunicationChannel; 1 public 1 private
-    #Reformat system to allow for proper message handling
-
-class PublicChannel(CommunicationChannel):
-    public = True
-    #TODO:Implement public channels
-
-class PrivateChannel(CommunicationChannel):
-    #Create a new private channel
-    def __init__(self, sender, recipient):
-        self.sender = sender
-        self.recipient = recipient
+@abs
+class CommsLink:
 
     def send(self, message):
-        self.recipient.perceiveMessage(message)
-        self.message_log.append(message)
-        message.sent = True
-
-    def reply(self, message):
-        self.sender.perceiveMessage(message)
-        self.message_log.append(message)
+        self.messageDest(message)
 
 
-class Message:
-    sent = False
+class PrivateLink(CommsLink):
 
-    def __init__(self, channel):
-        self.channel = channel
+    def __init__(self, messageDest):
+        self.messageDest = messageDest
+
+
+class PublicLink(CommsLink):
+
+    def __init__(self, connected_links):
+        self.connected_channels = connected_links
+
+    def messageDest(self, message):
+        for channel in self.connected_channels:
+            channel.messageDest(message)
